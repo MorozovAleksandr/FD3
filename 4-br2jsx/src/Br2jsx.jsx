cc0947be0ab86bbe310cfe99cea33af1f8ff1095
MorozovAleksandr <iamaleks.morozov@gmail.com>
2021-07-13 19:39:03 +0300

    refactoring

--- 4-br2jsx/src/Br2jsx.jsx
+++ 4-br2jsx/src/Br2jsx.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Br2jsx extends React.Component {
     render() {
         let createArray = this.props.text.replace(/<br? ?\/?>/g, ' <br> ').split(' ');
-        let replaceArray = createArray.map(item => item === '<br>' ? <br /> : item)
+        let replaceArray = createArray.map((item, index) => item === '<br>' ? <br key={index} /> : item);
         return (
             <div>
                 {replaceArray}
